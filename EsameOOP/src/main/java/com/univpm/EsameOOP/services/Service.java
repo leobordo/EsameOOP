42fe6000367a3a4482b261d49897fd67f825d177
sameonline <samueledisumma@gmail.com>
2021-12-15 16:03:19 +0100

    update service and serviceimplementation (2 new classes)

--- EsameOOP/src/main/java/com/univpm/EsameOOP/services/Service.java
+++ EsameOOP/src/main/java/com/univpm/EsameOOP/services/Service.java
@@ -5,6 +5,8 @@ import org.json.simple.JSONObject;
 public interface Service {
 
 	public abstract JSONObject getGeneralWeather(String city);
-	public String UNIXConverter(long time);
-	public JSONObject getVisibility(String city);
+	public abstract String UNIXConverter(long time);
+	public abstract JSONObject getVisibility(String city);
+	public abstract JSONObject getWind(String city);
+	public abstract JSONObject getVisibilityAndWind(String city);
 }
--- EsameOOP/src/main/java/com/univpm/EsameOOP/services/ServiceImplementation.java
+++ EsameOOP/src/main/java/com/univpm/EsameOOP/services/ServiceImplementation.java
@@ -1,5 +1,6 @@
 package com.univpm.EsameOOP.services;
 
+import com.univpm.EsameOOP.model.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.io.*;
@@ -8,19 +9,19 @@ import java.io.*;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.springframework.web.client.RestTemplate;
-
-
-public class ServiceImplementation implements Service {
+import org.springframework.stereotype.Service;
+@Service
+public class ServiceImplementation implements  com.univpm.EsameOOP.services.Service {
 
 	private String ApiKey="32469d04fb266e14e1d1f0d15a2599e8";
 	public JSONObject getGeneralWeather(String city)
 	{
-		JSONObject obj;
+
 		String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + ApiKey;
 		
 		RestTemplate rt = new RestTemplate();
 		
-		obj =new JSONObject(rt.getForObject(url, JSONObject.class));
+		JSONObject obj =new JSONObject(rt.getForObject(url, JSONObject.class));
 		
 		return obj;
 	}
@@ -36,11 +37,62 @@ public class ServiceImplementation implements Service {
 	{
 		JSONObject obj=getGeneralWeather(city);
 		int visibility=(int) obj.get("visibility");
-		long time=(long) obj.get("dt");
+		String country = (String) obj.get("country");
+	
+		int time=(int) obj.get("dt");
 		String formattedTime=UNIXConverter(time);
 		JSONObject obj2=new JSONObject();
 		obj2.put("Visibility", visibility);
+		
 		obj2.put("Date", formattedTime);
 		return obj2;
 	}
+	public JSONObject getWind(String city)
+	{
+		JSONObject obj=getGeneralWeather(city);
+		LinkedHashMap obj2=new LinkedHashMap<>();
+		obj2=(LinkedHashMap) obj.get("wind");
+		JSONObject obj3=new JSONObject();
+
+		double speed=(double) obj2.get("speed");
+		int degrees=(int) obj2.get("deg");
+		double gust=(double) obj2.get("gust");
+		
+        int time=(int) obj.get("dt");
+		String formattedTime=UNIXConverter(time);
+
+		obj3.put("Speed", speed);
+		obj3.put("Degrees", degrees);
+		obj3.put("Gust", gust);
+		obj3.put("Date", formattedTime);
+		return obj3;
+		
+	}
+	
+	public JSONObject getVisibilityAndWind(String city)
+	{
+		JSONObject obj=getGeneralWeather(city);
+		LinkedHashMap obj2=new LinkedHashMap<>();
+		obj2=(LinkedHashMap) obj.get("wind");
+		JSONObject obj3=new JSONObject();
+
+		double speed=(double) obj2.get("speed");
+		int degrees=(int) obj2.get("deg");
+		double gust=(double) obj2.get("gust");
+		
+        int time=(int) obj.get("dt");
+		String formattedTime=UNIXConverter(time);
+
+		int visibility=(int)obj.get("visibility");
+		obj3.put("Speed", speed);
+		obj3.put("Degrees", degrees);
+		obj3.put("Gust", gust);
+		obj3.put("Visibility",visibility);
+		obj3.put("Date", formattedTime);
+		return obj3;
+		
+	}
+	
+	
+	
 }
