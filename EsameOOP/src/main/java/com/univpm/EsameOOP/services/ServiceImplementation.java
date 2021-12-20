66b90a2f33c2ee7e6d9a4e0a2d2f9f912aac9cc0
Leonardo Bordoni <bordonileonardo@gmail.com>
2021-12-20 23:44:28 +0100

    service update(+readData)

--- EsameOOP/src/main/java/com/univpm/EsameOOP/services/ServiceImplementation.java
+++ EsameOOP/src/main/java/com/univpm/EsameOOP/services/ServiceImplementation.java
@@ -18,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class ServiceImplementation implements com.univpm.EsameOOP.services.Service {
 
 	private String ApiKey="32469d04fb266e14e1d1f0d15a2599e8";
+	
 	public JSONObject getGeneralWeather(String city)
 	{
 		JSONObject obj;
